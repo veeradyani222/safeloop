476657af54c871cde8ae9b406ad2b7018cf297c8
Veer Adyani <167308256+veeradyani222@users.noreply.github.com>
2024-09-02 09:32:28 +0530

    Update AddProduct.jsx

--- freeway-admin/src/Components/AddProduct/AddProduct.jsx
+++ freeway-admin/src/Components/AddProduct/AddProduct.jsx
@@ -31,7 +31,7 @@ const AddProduct = () => {
     let formData = new FormData();
     formData.append('product', image);
 
-    await fetch('http://localhost:4000/upload', {
+    await fetch('https://f-way.onrender.com/upload', {
       method: 'POST',
       headers: {
         Accept: 'application/json'
@@ -44,7 +44,7 @@ const AddProduct = () => {
     }
 
     console.log(product);  
-    await fetch('http://localhost:4000/addproduct',{
+    await fetch('https://f-way.onrender.com/addproduct',{
       method:'POST',
       headers:{
         Accept:'application/json',
