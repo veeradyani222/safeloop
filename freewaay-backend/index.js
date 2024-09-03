6e4c53627748bd1a554c4a697f7ac8e57d654eab
Veer Adyani <167308256+veeradyani222@users.noreply.github.com>
2024-09-03 13:50:45 +0530

    Update index.js

--- freewaay-backend/index.js
+++ freewaay-backend/index.js
@@ -37,7 +37,7 @@ app.use('/images', express.static(path.join(__dirname, 'upload/images')));
 app.post("/upload", upload.single('product'), (req, res) => {
     res.json({
         success: 1,
-        image_url: `http://localhost:${port}/images/${req.file.filename}`
+        image_url: `https://freeway-web.onrender.com/images/${req.file.filename}`
     });
 });
 
